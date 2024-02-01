9e6ce720fdfbe837314f0ea8feb7fb1e08c4e342
tomat7 <istarkov@yandex.ru>
2024-02-01 14:54:01 +0300

    bugfix on coloring

--- reg_func.cpp
+++ reg_func.cpp
@@ -17,28 +17,30 @@ void reg_print_name();
 
 void reg_init()
 {
-    cout << endl << "===== reg_init =====" << endl;
+  cout << endl
+       << "===== reg_init =====" << endl;
 
-    for (auto &D : PLCset)
-        for (auto &R : D.regs) {
-            string reg = (string)D.dev_name + "." + (string)R.rname;
-            REGmap[reg] = &R;
-            REGmap[reg]->rvalue = 5757;
-        }
+  for (auto &D : PLCset)
+    for (auto &R : D.regs)
+    {
+      string reg = (string)D.dev_name + "." + (string)R.rname;
+      REGmap[reg] = &R;
+      REGmap[reg]->rvalue = 5757;
+    }
 
-    /*  Old style cycles
-        int nb_plcs = static_cast<int>(PLCset.size());
-        cout << "Total PLCs: " << nb_plcs << endl;
-        for (int i = 0; i < nb_plcs; ++i) {             // Cycle for PLCs
-        for (int j = 0; j < PLCset[i].nb_regs; ++j) { // Cycle for REGs
-          PLC &D = PLCset[i];
-          reg_init_name(D.dev_name, R.rname, &R.rvalue);
-        }
-        }
-        //  int nb_regs = static_cast<int>(PLCset[i].regs.size());  // old style
-    */
+  /*  Old style cycles
+      int nb_plcs = static_cast<int>(PLCset.size());
+      cout << "Total PLCs: " << nb_plcs << endl;
+      for (int i = 0; i < nb_plcs; ++i) {             // Cycle for PLCs
+      for (int j = 0; j < PLCset[i].nb_regs; ++j) { // Cycle for REGs
+        PLC &D = PLCset[i];
+        reg_init_name(D.dev_name, R.rname, &R.rvalue);
+      }
+      }
+      //  int nb_regs = static_cast<int>(PLCset[i].regs.size());  // old style
+  */
 
-    return;
+  return;
 }
 
 /*
@@ -51,19 +53,22 @@ void reg_init()
 
 void reg_print_name()
 {
-    cout << endl << "======= regs_print_name =======" << endl;
-    const char* C = KNRM;
+  cout << endl
+       << "======= regs_print_name =======" << endl;
 
-    for (const auto &[rn, r] : REGmap) {
-        if (r->rstatus < 0)
-            C = KRED;
-        if (strcmp(r->rtype, "i") == 0)
-            printf("%s%-12s %7d\n" NRM, C, rn.c_str(), r->rvalue);
-        else
-            printf("%s%-12s %7.2f\n" NRM, C, rn.c_str(), (int16_t)r->rvalue * 0.01);
-    }
+  for (const auto &[rn, r] : REGmap)
+  {
+    const char *C = KNRM;
+    if (r->rstatus < 0)
+      C = KRED;
 
-    return;
+    if (strcmp(r->rtype, "i") == 0)
+      printf("%s%-12s %7d\n" NRM, C, rn.c_str(), r->rvalue);
+    else
+      printf("%s%-12s %7.2f\n" NRM, C, rn.c_str(), (int16_t)r->rvalue * 0.01);
+  }
+
+  return;
 }
 
 /*
