b5e42353ba1b7ad1508b42f2b84370a06c2f77a3
tomat7 <istarkov@yandex.ru>
2024-10-01 17:55:36 +0300

    again folders 2

--- include/opc/opc_add.cpp
+++ include/opc/opc_add.cpp
@@ -59,6 +59,7 @@ void OpcServer_c::addVar(string s, int16_t i16, int rmode, string folder)
 void OpcServer_c::addVar(string s, int64_t i64, int rmode, string folder)
 {
   addVar_Names(s, UA_TYPES_INT64, rmode, folder);
+  addVar_NodeId(vars[s]);
   vars[s].value.i64 = i64;
   vars[s].ptr_value = &vars[s].value.i64;
   addVariable(vars[s]);
@@ -68,6 +69,7 @@ void OpcServer_c::addVar(string s, int64_t i64, int rmode, string folder)
 void OpcServer_c::addVar(string s, uint16_t ui16, int rmode, string folder)
 {
   addVar_Names(s, UA_TYPES_UINT16, rmode, folder);
+  addVar_NodeId(vars[s]);
   vars[s].value.ui16 = ui16;
   vars[s].ptr_value = &vars[s].value.ui16;
   addVariable(vars[s]);
@@ -94,6 +96,7 @@ void OpcServer_c::addVar(string s, uint16_t ui16, int rmode, string folder)
 void OpcServer_c::addVar(string s, float fl, int rmode, string folder)
 {
   addVar_Names(s, UA_TYPES_FLOAT, rmode, folder);
+  addVar_NodeId(vars[s]);
   vars[s].value.fl = fl;
   vars[s].ptr_value = &vars[s].value.fl;
   addVariable(vars[s]);
