2c253722abe36cc922c27fe38d8a6426df0239a1
tomat7 <istarkov@yandex.ru>
2025-03-18 18:26:08 +0300

    save 8

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 // Copyright 2024 Tomat7 (star0413@gmail.com)
 // https://31.44.7.12:55972/panel/
 // http://85.117.235.21:55971/
+// test!
 
 #define CFG_DIR "/var/tmp/conf"
 #define CFG_FILE "modbus.cfg"
