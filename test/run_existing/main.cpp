6d89eef02e7b8d43b73d767cf5f8658f5419c985
tomat7 <istarkov@yandex.ru>
2025-03-24 15:28:43 +0300

    prog_source updated

--- test/run_existing/main.cpp
+++ test/run_existing/main.cpp
@@ -11,7 +11,7 @@
  
 int main()
 {
-    std::string cmdline = PING_PROG + std::string(PING_OPT) + PING_ADDR + ">" +PING_OUTFILE;
+    std::string cmdline = PING_PROG + std::string(PING_OPT) + PING_ADDR + ">" + PING_OUTFILE;
 //    std::system("ping -c5 1.1.1.1 >OUT_FILE"); // executes the UNIX command "ls -l >test.txt"
     std::system(cmdline.c_str()); // executes the UNIX command 
     std::cout << std::ifstream(PING_OUTFILE).rdbuf();
