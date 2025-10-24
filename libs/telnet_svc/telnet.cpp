1b8dffa2a482d3dc5516524c68afa11816db133c
tomat7 <istarkov@yandex.ru>
2025-10-24 17:02:01 +0300

    telnet class rework

--- libs/telnet_svc/telnet.cpp
+++ libs/telnet_svc/telnet.cpp
@@ -11,125 +11,184 @@
 
 #include "telnet.h"
 
-int TelnetSvc_c::init_socket()
+int TelnetSvc_c::_init_socket()
 {
   // 1. Create a listening socket
-  server_socket = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_socket < 0) {
-    std::cerr << "Error creating listening socket" << std::endl;
+  server_socket_ = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket_ < 0) {
+    std::cerr << "Error creating socket" << std::endl;
     return 1;
   }
 
-  // Set socket options to reuse address
-  int opt = 1;
-  if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-    std::cerr << "Error setting socket options" << std::endl;
-    close(server_socket);
-    return 1;
-  }
+  return 0;
+}
 
-};
-
-int telnet_fn()
+int TelnetSvc_c::_set_socket()
 {
+  // 2. Set socket options to reuse address (??)
+  int opt = 1;
+  if (setsockopt(server_socket_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+    std::cerr << "Error setting socket options" << std::endl;
+    close(server_socket_);
+    return 1;
+  }
 
-  init();
+  return 0;
+}
 
 
-  // Bind the listening socket to an address and port
+int TelnetSvc_c::_bind_socket()
+{
+  // 3. Bind the listening socket to an address and port
   sockaddr_in server_addr;
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(PORT);
+  server_addr.sin_port = htons(port_);
 
-  if (bind(server_socket, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+  if (bind(server_socket_, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
     std::cerr << "Error binding socket" << std::endl;
-    close(server_socket);
+    close(server_socket_);
     return 1;
   }
 
+  return 0;
+}
+
+int TelnetSvc_c::_listen_socket()
+{
   // Listen for incoming connections
-  if (listen(server_socket, 5) < 0) {
+  if (listen(server_socket_, 5) < 0) {
     std::cerr << "Error listening on socket" << std::endl;
-    close(server_socket);
+    close(server_socket_);
     return 1;
   }
 
-  std::cout << "Server listening on port " << PORT << std::endl;
+  return 0;
+}
 
-  // Initialize pollfd structures
-  std::vector<pollfd> array_fds(MAX_CLIENTS + 1); // +1 for the listening socket
-  int nb_active_fds = 0; // Number of active file descriptors
+
+int TelnetSvc_c::init()
+{
+  active_fds.resize(max_clients_ + 1);
+  _init_socket();
+  _set_socket();
+
+  _bind_socket();
+
+  _listen_socket();
+
+  std::cout << "Server listening on port: " << port_
+            << ", socket: " << server_socket_ << std::endl;
+
+  return 0;
+}
+
+
+int TelnetSvc_c::run()
+{
+  // Reset Number of active file descriptors
+  nb_active_fds_ = 0;
 
   // Add the listening socket to the pollfd array
-  array_fds[0].fd = server_socket;
-  array_fds[0].events = POLLIN; // Monitor for incoming data (new connections)
-  nb_active_fds++;
+  active_fds[0].fd = server_socket_;
+  active_fds[0].events = POLLIN; // Monitor for incoming data (new connections)
+  nb_active_fds_++;
 
-  // Main polling loop
-  while (true) {
-    int sum_updated_fds = poll(array_fds.data(), nb_active_fds, -1); // -1 for infinite timeout
+  std::cout << "Server running: " << active_fds.size() << " "
+            << active_fds.capacity() << std::endl;
+
+// Main polling loop
+  while (true) {                           // -1 for infinite timeout
+    int sum_updated_fds = poll(active_fds.data(), nb_active_fds_, -1);
     if (sum_updated_fds < 0) {
       std::cerr << "Error in poll()" << std::endl;
       break;
     }
 
     // Check if the listening/server socket has an event (new connection)
-    if (array_fds[0].revents & POLLIN) {
-      sockaddr_in client_addr;
-      socklen_t client_len = sizeof(client_addr);
-      int client_sock = accept(server_socket, (sockaddr*)&client_addr, &client_len);
-      if (client_sock < 0)
-        std::cerr << "Error accepting connection" << std::endl;
-      else {
-        if (nb_active_fds < MAX_CLIENTS + 1) {
-          array_fds[nb_active_fds].fd = client_sock;
-          array_fds[nb_active_fds].events = POLLIN; // Monitor for incoming data from client
-          nb_active_fds++;
-          std::cout << "New connection from " << inet_ntoa(client_addr.sin_addr)
-                    << ":" << ntohs(client_addr.sin_port) << std::endl;
-        } else {
-          std::cerr << "Max clients reached, closing new connection" << std::endl;
-          close(client_sock);
-        }
-      }
+    if (active_fds[0].revents & POLLIN) {
+      _new_client();
       sum_updated_fds--; // Decrement count as we handled this (server's) event
     }
 
     // Check for events on client sockets
-    for (int i = 1; ((i < nb_active_fds) && (sum_updated_fds > 0)); ++i) {
-      if (array_fds[i].revents & POLLIN) {
-        char buffer[1024];
-        ssize_t bytes_read = recv(array_fds[i].fd, buffer, sizeof(buffer) - 1, 0);
+    for (int i = 1; ((i < nb_active_fds_) && (sum_updated_fds > 0)); ++i) {
+      if (active_fds[i].revents & POLLIN) {
+        ssize_t bytes_read = recv(active_fds[i].fd, buffer, sizeof(buffer) - 1, 0);
 
         if (bytes_read <= 0) { // Connection closed or error
-
-          if (bytes_read == 0)
-            std::cout << "Client disconnected: " << array_fds[i].fd << std::endl;
-          else
-            std::cerr << "Error reading from client: " << array_fds[i].fd << std::endl;
-          close(array_fds[i].fd);
-          // Remove the disconnected client from the array
-          for (int j = i; j < nb_active_fds - 1; ++j)
-            array_fds[j] = array_fds[j + 1];
-          nb_active_fds--;
+          _bad_client(i);
           i--; // Re-check the current index as it now contains a new fd
-        } else {
-          buffer[bytes_read] = '\0';
-          std::cout << "Received from client " << array_fds[i].fd << ": " << buffer << std::endl;
-          // Echo back the received data
-          send(array_fds[i].fd, buffer, bytes_read, 0);
-        }
+        } else
+          _handle_client(i, bytes_read);
+
         sum_updated_fds--;
       }
     }
   }
 
-  // Close all open sockets (not strictly necessary in this infinite loop example, but good practice)
-  for (int i = 0; i < nb_active_fds; ++i)
-    close(array_fds[i].fd);
+  return 0;
+}
 
+int TelnetSvc_c::_handle_client(int i, ssize_t bytes_read)
+{
+  buffer[bytes_read] = '\0';
+  std::cout << "Received from client " << active_fds[i].fd << ": " << buffer << std::endl;
+  // Echo back the received data
+  send(active_fds[i].fd, buffer, bytes_read, 0);
+
+  return 0;
+}
+
+
+int TelnetSvc_c::_new_client()
+{
+  int rc = 0;
+  sockaddr_in client_addr;
+  socklen_t client_len = sizeof(client_addr);
+  int client_sock = accept(server_socket_, (sockaddr*)&client_addr, &client_len);
+
+  if (client_sock < 0)
+    std::cerr << "Error accepting connection" << std::endl;
+  else {
+    if (nb_active_fds_ < max_clients_ + 1) {
+      active_fds[nb_active_fds_].fd = client_sock;
+      active_fds[nb_active_fds_].events = POLLIN; // Monitor for incoming data from client
+      nb_active_fds_++;
+      rc = 1;
+      std::cout << "New connection from " << inet_ntoa(client_addr.sin_addr)
+                << ":" << ntohs(client_addr.sin_port) << std::endl;
+    } else {
+      std::cerr << "Max clients reached, closing new connection" << std::endl;
+      close(client_sock);
+    }
+  }
+
+  return rc;
+}
+
+int TelnetSvc_c::_bad_client(int i)
+{
+  std::cout << "Client error/disconnected: " << active_fds[i].fd << std::endl;
+  close(active_fds[i].fd);
+
+  // Remove the disconnected client from the array
+  for (int j = i; j < nb_active_fds_ - 1; ++j)
+    active_fds[j] = active_fds[j + 1];
+
+  nb_active_fds_--;
+
+  return 0;
+}
+
+
+int TelnetSvc_c::stop()
+{
+  // Close all open sockets (good practice)
+  for (int i = 0; i < nb_active_fds_; ++i)
+    close(active_fds[i].fd);
+  active_fds.clear();
   return 0;
 }
 
--- libs/telnet_svc/telnet.h
+++ libs/telnet_svc/telnet.h
@@ -21,22 +21,33 @@ using std::mutex;
 class TelnetSvc_c
 {
 public:
-  TelnetSvc_c(int _p = PORT_, int _maxcl = MAX_CLIENTS_)
+  // Initialize pollfd structures (+1 for listening socket)
+  TelnetSvc_c(uint16_t _p = PORT_, int _maxcl = MAX_CLIENTS_)
     : port_(_p), max_clients_(_maxcl) {};
 
+  ~TelnetSvc_c() { stop(); };
+
   int init();
+  int run();
+  int stop();
 
 private:
-  int port_ = PORT_;
+  int _init_socket();
+  int _set_socket();
+  int _bind_socket();
+  int _listen_socket();
+
+  int _new_client();
+  int _bad_client(int);
+  int _handle_client(int, ssize_t);
+//  int _handle_socket();
+
+  uint16_t port_ = PORT_;
   int max_clients_ = MAX_CLIENTS_;
-  int server_socket;
-  int nb_active_fds = 0; // Number of active file descriptors
-  int nb_updated_fds;
-
-  sockaddr_in server_addr;
-
-  std::vector<pollfd> array_fds; //(MAX_CLIENTS_ + 1); // +1 for the listening socket
-
+  int server_socket_;
+  int nb_active_fds_ = 0; // Number of active file descriptors
+  char buffer[1024];
+  std::vector<pollfd> active_fds;
 
 };
 
